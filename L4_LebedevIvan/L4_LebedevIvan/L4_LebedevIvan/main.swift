61005f52970fa0599deb8e986c2f2d119fea6ca3
bomshhh <bomshhh@yandex.ru>
2018-12-05 18:07:05 +0300

    Выполнил ДЗ 4

---
+++ L4_LebedevIvan/L4_LebedevIvan/L4_LebedevIvan/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L4_LebedevIvan
+//
+//  Created by Иван Лебедев on 05/12/2018.
+//  Copyright © 2018 Иван Лебедев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
