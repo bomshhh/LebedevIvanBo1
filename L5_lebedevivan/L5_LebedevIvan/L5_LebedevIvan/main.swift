839e09ca78e6e816aab9f8e93de589eccf69bb1c
bomshhh <bomshhh@yandex.ru>
2018-12-06 15:34:13 +0300

    DZ5

---
+++ L5_lebedevivan/L5_LebedevIvan/L5_LebedevIvan/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L5_LebedevIvan
+//
+//  Created by Иван Лебедев on 06/12/2018.
+//  Copyright © 2018 Иван Лебедев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
